94495fe400701ce9e8225546e2d40b38846f261f
Adam Brittain <adsy.britt@gmail.com>
2023-05-13 12:43:18 +1000

    Started work on login form

--- localsound.frontend/src/features/Authentication/Login.tsx
+++ localsound.frontend/src/features/Authentication/Login.tsx
@@ -1,21 +1,17 @@
+import { Button } from "react-bootstrap";
+
 const Login = () => {
   return (
-    <div>
-      <div>Facebook login widget</div>
-      <div>Soundcloud login widget</div>
-      <div>input field</div>
-      <div>Facebook login widget</div>
-      <div>Soundcloud login widget</div>
-      <div>input field</div>
-      <div>Facebook login widget</div>
-      <div>Soundcloud login widget</div>
-      <div>input field</div>
-      <div>Facebook login widget</div>
-      <div>Soundcloud login widget</div>
-      <div>input field</div>
-      <div>Facebook login widget</div>
-      <div>Soundcloud login widget</div>
-      <div>input field</div>
+    <div id="auth-modal">
+      <div className="d-flex flex-row mb-2 header">
+        <h2 className="header-title ml-1 mt-1 align-self-center">
+          Login to your account
+        </h2>
+      </div>
+      <div className="d-flex header justify-content-end mt-2">
+        <Button className="purple-button mr-2">I play the music</Button>
+        <Button className="purple-button">I listen to the music</Button>
+      </div>
     </div>
   );
 };
--- localsound.frontend/src/features/LandingPage/LandingPageFunctionality.tsx
+++ localsound.frontend/src/features/LandingPage/LandingPageFunctionality.tsx
@@ -1,17 +1,12 @@
 import { Row } from "react-bootstrap";
-import LandingPageSearch from "./LandingPageSearch";
-import Carousel from "./Carousel";
 
 const LandingPageFunctionality = () => {
   return (
     <Row>
-      <div className="container">
-        <LandingPageSearch />
-      </div>
-      {/* <div className="container">Filter artists based on location ability</div>
+      <div className="container">Filter artists based on location ability</div>
       <div className="container">Trending artists based on location?</div>
       <div className="container">Feature</div>
-      <div className="container">Feature`</div> */}
+      <div className="container">Feature`</div>
     </Row>
   );
 };
